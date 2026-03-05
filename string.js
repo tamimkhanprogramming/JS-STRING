efa5870e9183c804911618e46ca465eb6bf7ae31
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-05 16:17:52 +0600

    Updated

--- string.js
+++ string.js
@@ -6,9 +6,20 @@ const district = 'Neelam Valley';
 
 const Police_Station = new String('Jehlum Vally');
 
-console.log(typeof country);
+// console.log(typeof country);
 
-console.log(Police_Station);
+// console.log(Police_Station);
+
+const numbers = [54,98,78,21,65];
+console.log(numbers);
+numbers[1] = 11;
+console.log(numbers);
+// const capital = 'DhaKa';
+
+// console.log(capital.length);
+
+// console.log(capital[4]);
+
 
 
 
