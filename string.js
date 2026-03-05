656c988266c2de00533d616620adab55fed39951
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-05 16:05:25 +0600

    Updated

---
+++ string.js
@@ -0,0 +1,16 @@
+const country = 'Pakistan';
+
+const division = 'Azad Kashmir';
+
+const district = 'Neelam Valley';
+
+const Police_Station = new String('Jehlum Vally');
+
+console.log(typeof country);
+
+console.log(Police_Station);
+
+
+
+
+
