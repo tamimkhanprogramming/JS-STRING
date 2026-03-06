d5e904b5decf06bd0bb91c1dc6268c6f3edd8069
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-06 16:28:35 +0600

    Updated

---
+++ lowercase-uppercase.js
@@ -0,0 +1,52 @@
+const school = 'Mern Sun School & College';
+
+console.log(school);
+console.log(school.toLocaleLowerCase());
+console.log(school.toLocaleUpperCase());
+
+const book = 'chemistry';
+
+const Boi = 'Chemistry';
+
+// Uppercase: ABCD EFG
+// lowercase: abcd efg
+if(book === Boi){
+
+    console.log("Porikkhai Bina Hisave Pass");
+}
+else{
+    console.log("Better Luck Next Tym");
+}
+
+const drink = ' Water ';
+
+const liquid = ' Water ';
+
+if(drink.trim()===liquid.trim()){
+     console.log("Pani Pora Dao");
+}
+else{
+
+    console.log("Pani Khaio Na");
+
+}
+
+const schools = "Mern Sun School & College Batch 16";
+
+// console.log(school.toLocaleUpperCase());
+
+console.log(school.toLocaleLowerCase());
+
+const subject = "Chemistry";
+
+const boi = "Chemistry";
+
+if(subject===boi){
+
+    console.log("Bina Hisave Jannat a Jaba");
+}
+else{
+
+    console.log("Namaz Poro Na Kivabe Jannat a Jaba");
+}
+
