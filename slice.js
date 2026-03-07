aecab220a3202c75d04cf57b0ea7906f340bd41d
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-07 12:39:53 +0600

    Updated

---
+++ slice.js
@@ -0,0 +1,26 @@
+// const address = 'Andorkilla';
+
+// const part = address.slice(2,5);
+
+// console.log(part);
+
+// const address = 'Andorkilla';
+
+// const part = address.slice(2,5);
+
+// console.log(part);
+
+const sentence = "I am a Good Boy";
+
+console.log(sentence.split('a'));
+
+const friendsStr = 'Sun Vally,Cosmo Village,Shotru,Hingshuk';
+
+const friend = friendsStr.split(',');
+console.log(friend);
+
+const realFriend = ['Muntasir','Samin','Mahmud','Shamim','Mansur','Harris Harras','Zahjah'];
+
+console.log(realFriend.join());
+console.log(realFriend.join('|'));
+
