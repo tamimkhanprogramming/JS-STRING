90c588f5bcec08b3f186edfbf9f00bec665d5b64
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-09 13:07:42 +0600

    Updated

--- slice.js
+++ slice.js
@@ -1,16 +1,37 @@
-const address = "Bangladesh";
+// const address = "Bangladesh";
 
-const realAddress = address.slice(2,5);
+// const realAddress = address.slice(2,5);
 
-console.log(realAddress);
+// console.log(realAddress);
 
-const friendStr = ['Muntasir','Mahmud','Shamim','Shoab','Harris','Zahjah'];
+// const friendStr = ['Muntasir','Mahmud','Shamim','Shoab','Harris','Zahjah','Mansur'];
 
-console.log(friendStr.join('-'));
+// console.log(friendStr.join('-'));
 
-const sentence = "I am Learning Web Development";
+// const sentence = "I am Learning Web Development";
+
+// const new_sentence = sentence.split(' ');
+
+// console.log(new_sentence);
+
+const nephew = ['Nusaiba Rahman Khan','Musab Rahman Khan'];
+
+console.log(nephew.join('|'));
+
+
+const address = 'Bangladesh';
+
+const conquer = address.slice(2,5);
+
+console.log(conquer);
+
+const sentence = 'I am Learning Web Development';
 
 const new_sentence = sentence.split(' ');
 
 console.log(new_sentence);
+
+const brotherHood = ['Muntasir','Mahmud','Shamim','Mansur','Shoab','Harris','Zahjah'];
+
+console.log(brotherHood.join('-'));
 
