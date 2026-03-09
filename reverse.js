768d459bf08e1fe51a3103ab927cc496b3422f31
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-09 16:00:57 +0600

    Updated

---
+++ reverse.js
@@ -0,0 +1,37 @@
+// const sentence = 'I am Learning Web Development From Programming Hero';
+
+// //  let reverse = ' ';
+// // for(const letter of sentence){
+
+// //     reverse = letter+reverse;
+    
+// // }
+// // console.log(reverse);
+
+// //Shortcut Solution
+// const reverse = sentence.split('').reverse('').join('');
+
+// console.log(reverse);
+
+const sentence = "I am Learning Web Development at Programming Hero Platform";
+
+let reverse = '';
+
+for(const letter of sentence){
+
+    reverse = letter+reverse;
+}
+console.log(reverse);
+
+// const letter = "I am Learning Web Development Course From Programming Hero Platform";
+
+// reverse = letter.split('').reverse().join('');
+
+// console.log(reverse);
+
+const letter = "I am Learning Web Development Course From Programming Hero Platform";
+
+reverse = letter.split('').reverse().join('');
+
+console.log(reverse);
+
