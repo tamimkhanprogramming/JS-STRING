8d173163d077b808eee39588ed5dd7edc1c3d4bd
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-11 15:14:09 +0600

    Push

---
+++ object.js
@@ -0,0 +1,21 @@
+const bootle ={
+
+    brand: 'Apple',
+    price: 45,
+    color: 'White',
+    isClean: false,
+
+}
+
+const subject = {
+
+    name: 'Biology',
+    teacher: 'rasheda mam',
+    examDate: '30 Feb',
+    chapters: ['First','Second','Third'],
+    exams:{
+        name: 'Final Exam',
+        marks: 100,
+
+    }
+}
