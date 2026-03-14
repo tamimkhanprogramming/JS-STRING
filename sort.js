c355c110331f9b97ea4ebdcff6c89f78c238bf9a
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-14 15:40:18 +0600

    Updated

--- sort.js
+++ sort.js
@@ -1,29 +1,18 @@
-// const persons = ['rakib','nokib','sakib','akib','dakib'];
-
-// const sortedPersons = persons.sort();
-
-// console.log(sortedPersons);
-
 // String Wise Sorting
-// const persons = ['rakib','nokib','sakib','akib','dakib'];
+const persons1 = ['rakib','nokib','sakib','akib','dakib'];
 
-// const sortedPersons = persons.sort();
+const sorted_Persons = persons1.sort();
 
-// console.log(sortedPersons);
+console.log(sorted_Persons);
+
 
 // Asceding Wise Sorting
-// const numbers = [4,7,12,8,43,6,1];
 
-// const numbers_asc = numbers.sort(function(a,b){return a-b});
+const numbers = [4,7,12,8,43,6,1];
 
-// console.log(numbers_asc);
+const numbers_asc = numbers.sort(function(a,b){return a-b});
 
-// const numbers = [43,12,1,3,5,6,8];
-
-// const numbers_asc = numbers.sort(function(a,b){return a-b});
-
-// console.log(numbers_asc);
-
+console.log(numbers_asc);
 
 const PersonName = ['Donald Trump','Netaniyahu','Putin','Xi-Jing-Ping','Kim-Jong-Un'];
 
@@ -31,9 +20,9 @@ const Competitor_Person_Mahdi = PersonName.sort();
 
 console.log(Competitor_Person_Mahdi);
 
-const numbers = [12,11,10,9,8,7,6,5,4,3,2,1];
+const numbers1 = [12,11,10,9,8,7,6,5,4,3,2,1];
 
-const Change_Numbers = numbers.sort(function(a,b){return a-b});
+const Change_Numbers = numbers1.sort(function(a,b){return a-b});
 
 console.log(Change_Numbers);
 
